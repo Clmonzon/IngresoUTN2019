e6799ceda2a3c7343ba0a5d5658267cf3b849cb9
Clmonzon <47321401+Clmonzon@users.noreply.github.com>
2019-02-04 12:28:47 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostrar()
 {
+    var nombre;
+
+    nombre= prompt ("El nombre es") 
 	
+    alert("Su nombre es " + nombre   )
 }
 
--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,11 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostrar()
 {	
-	
+var nombre;
+var edad;
+
+nombre = document.getElementById("elNombre").value;
+edad = document.getElementById("laEdad").Value;
+alert("Usted se llama " + nombre + "y tiene" edad");
 }
 
