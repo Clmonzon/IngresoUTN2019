e12a4b6f370490c84d736765bdc61f8a3b115ccb
Clmonzon <47321401+Clmonzon@users.noreply.github.com>
2019-02-18 12:24:08 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,25 @@
 function Mostrar()
 {
-//tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño = document.getElementById("mes").value;
+
+    switch(mesDelAño){
+        case "Febrero":
+            alert("Este mes no tiene mas de 29 dìas");
+            break;
+        case "Enero":
+        case "Marzo":
+        case "Abril":
+        case "mayo":
+        case "Junio":
+        case "Julio":
+        case "Agosto":
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre":
+        alert("Este mes tiene mas de 30 dìas");
+        break;
+        }
 
 //alert (mesDelAño);
-	
-	
-
-
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,26 @@
 function Mostrar()
 {
-//tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
 
+    switch(mesDelAño){
+        case "Febrero":
+            alert("Tiene 28 dìas");
+            break;
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert("Tienen 30 dìas");
+            break;
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "Juio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":
+            alert("Tienen 31 dìas");
+            break;
+    }
 //alert (mesDelAño);
-	
-	
-
-
-
 }//FIN DE LA FUNCIÓN
