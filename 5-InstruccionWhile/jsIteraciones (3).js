d6a8f4b9ea2e26951534470249569fd9a6fbf69e
Clmonzon <47321401+Clmonzon@users.noreply.github.com>
2019-02-25 00:59:26 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,9 @@
 function Mostrar()
 {
-
-var clave = prompt("ingrese el número clave.");
-
+var clave = prompt("Ingrese el número clave.");
+    while(clave != "utn750"){
+        clave = prompt("Clave incorrecta");
+    }
+    alert("Clave correcta.");
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,9 @@
 function Mostrar()
 {
-
-	var numero = prompt("ingrese un número entre 0 y 10.");
-
+var numero = prompt("Ingrese un número entre 0 y 10.");
+	while(numero < 0 || numero > 10 || isNaN(numero)){
+		numero = parseInt(prompt("No es numero entre 0 y 9"));
+	}
+document.getElementById("Numero").value = numero;
 
 }//FIN DE LA FUNCIÓN
